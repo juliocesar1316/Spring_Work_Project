ef3e4ace59ecacbe01d600a9b9c15c02836be4f3
julio cesar martins <juliocesar1316@gmail.com>
2023-09-27 23:08:55 -0300

    teste

---
+++ src/main/java/com/example/work_project/WorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.work_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/work_project/WorkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
